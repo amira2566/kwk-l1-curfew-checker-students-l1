1962ab30d97c1ddd725e59cc388d4d5517fe6573
amira2566 <braemira2@icloud.com>
2018-06-05 21:40:00 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,32 @@
 def simple_curfew_checker(time)
-  # code goes here
+if time==11
+  puts "on time"
 end
+end
+puts simple_curfew_checker(11)
 
 def curfew_checker(time)
-  # code goes here
+if time==11
+  puts "your in trouble"
+else time<12
 end
+end
+puts curfew_checker(10)
 
 def complex_curfew_checker(time)
-  # code goes here
+if time<10
+  puts "early"
+elsif time==11
+puts "on the dot"
+else "eleven is not less than 10 and eleven does not equal eleven"
 end
+end
+puts complex_curfew_checker
 
 def deluxe_curfew_checker(time)
-  # code goes here
-end
+  puts "It is" + #{time}".
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
+  puts 
+end
 end
